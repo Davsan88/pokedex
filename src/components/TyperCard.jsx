be85a66ffe9bd4cd5d402cf63f1b1bb36b08eb50
Dav <david.sanchez.martinez@outlook.com>
2025-01-21 20:52:39 +0000

    Add tiles to PokeCards

--- src/components/TyperCard.jsx
+++ src/components/TyperCard.jsx
@@ -1,6 +1,15 @@
-const TyperCard = () => {
+import { pokemonTypeColors } from "../utils"
+
+const TyperCard = ({ type }) => {
   return (
-    <div></div>
+    <div 
+      className="type-tile" 
+      style={{ 
+          color: pokemonTypeColors?.[type]?.color, 
+          background: pokemonTypeColors?.[type]?.background 
+      }}>
+      <p>{type}</p>
+    </div>
   )
 }
 
