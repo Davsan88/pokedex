339b0921b692d1678f84d47d6eefde2ef9c14a2b
Dav <david.sanchez.martinez@outlook.com>
2025-01-21 19:45:56 +0000

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,13 +6,13 @@ export const SideNav = () => {
 
   return (
     <nav>
-      <div  className={"header"}>
+      <div  className={'header'}>
         <h1 className="text-gradient">Pokédex</h1>
         <input />
       </div>
       {first151Pokemon.map((pokemon, pokemonIndex) => {
         return(
-          <button className={'nav-card'}>
+          <button key={pokemonIndex} className={'nav-card'}>
             <p>{getFullPokedexNumber(pokemonIndex)}</p>
             <p>{pokemon}</p>
           </button>
