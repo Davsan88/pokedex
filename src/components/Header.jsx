e9160f0b055deefe0b3590917225a33715eb2c8f
Dav <david.sanchez.martinez@outlook.com>
2025-01-29 20:55:59 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
       <button className="open-nav-button">
         <i class="fa-solid fa-bars"></i>
       </button>
+      <h1 className="text-gradient">Pokédex</h1>
     </header>
   )
 }
