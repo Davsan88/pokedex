cbb01991eb08121ebd5b0d22bdf3d63a6246d45e
Dav <david.sanchez.martinez@outlook.com>
2025-01-30 11:49:08 +0000

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,7 @@
 import { first151Pokemon, getFullPokedexNumber } from "../utils"
 import { useState } from "react"
 
-const SideNav = ({ selectedPokemon, setSelectedPokemon }) => {
+const SideNav = ({ selectedPokemon, setSelectedPokemon, handleToggleMenu, showSideMenu }) => {
   const [searchValue, setSearchValue] = useState('')
  
   const filteredPokemon = first151Pokemon.filter((element, elementIndex) => {
@@ -19,12 +19,15 @@ const SideNav = ({ selectedPokemon, setSelectedPokemon }) => {
     <nav>
       <div  className={'header'}>
         <h1 className="text-gradient">Pokédex</h1>
+        <button className="open-nav-button">
+          <i className="fa-solid fa-arrow-left-long"></i>
+        </button>
+      </div>
         <input 
           placeholder="E.g. 001 or Bulba..."
           input={searchValue}
           onChange={(e) => {setSearchValue(e.target.value)}} 
         />
-      </div>
       {filteredPokemon.map((pokemon, pokemonIndex) => {
         const truePokedexNumber = first151Pokemon.indexOf(pokemon)
         return(
