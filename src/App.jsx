6f60d5bb4d60b0c878f82b622039c1386b275ee1
Dav <david.sanchez.martinez@outlook.com>
2025-01-30 11:25:00 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,21 @@ import { useState } from "react"
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(0)  
+  const [showSideMenu, setShowSideMenu] = useState(false)
+
+  function handleToggleMenu() {
+    setShowSideMenu(!showSideMenu)
+  }
 
   return (
     <>
-      <Header />
+      <Header 
+        handleToggleMenu={handleToggleMenu}
+      />
       <SideNav 
         selectedPokemon={selectedPokemon} 
         setSelectedPokemon={setSelectedPokemon}
+        handleToggleMenu={handleToggleMenu}
       />
       <PokeCard 
         selectedPokemon={selectedPokemon}
