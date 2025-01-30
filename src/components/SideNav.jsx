84bf79bdf31588a3c25e588b9f383d7f3e57e238
Dav <david.sanchez.martinez@outlook.com>
2025-01-30 13:13:33 +0000

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,11 +3,11 @@ import { useState } from "react"
 
 const SideNav = ({ selectedPokemon, setSelectedPokemon, handleToggleMenu, showSideMenu }) => {
   const [searchValue, setSearchValue] = useState('')
- 
+
   const filteredPokemon = first151Pokemon.filter((element, elementIndex) => {
     // if full pokedex number includes the current search value, return true
     if ((getFullPokedexNumber(elementIndex)).includes(searchValue)) { return true }
-    
+
     // if full pokedex name includes the current search value, return true
     if (element.toLowerCase().includes(searchValue.toLowerCase())) { return true }
 
@@ -16,24 +16,24 @@ const SideNav = ({ selectedPokemon, setSelectedPokemon, handleToggleMenu, showSi
   })
 
   return (
-    <nav className={`${!showSideMenu ? "open" : ""}`}>  
-      <div  className={`header ${!showSideMenu ? "open" : ""}`}>
-        <button className="open-nav-button">
+    <nav className={`${!showSideMenu ? "open" : ""}`}>
+      <div className={`header ${!showSideMenu ? "open" : ""}`}>
+        <button onClick={handleToggleMenu} className="open-nav-button">
           <i className="fa-solid fa-arrow-left-long"></i>
         </button>
         <h1 className="text-gradient">Pokédex</h1>
       </div>
-        <input 
-          placeholder="E.g. 001 or Bulba..."
-          input={searchValue}
-          onChange={(e) => {setSearchValue(e.target.value)}} 
-        />
+      <input
+        placeholder="E.g. 001 or Bulba..."
+        input={searchValue}
+        onChange={(e) => { setSearchValue(e.target.value) }}
+      />
       {filteredPokemon.map((pokemon, pokemonIndex) => {
         const truePokedexNumber = first151Pokemon.indexOf(pokemon)
-        return(
-          <button 
-            onClick={() => {setSelectedPokemon(truePokedexNumber)}}
-            key={pokemonIndex} 
+        return (
+          <button
+            onClick={() => { setSelectedPokemon(truePokedexNumber) }}
+            key={pokemonIndex}
             className={'nav-card' + (truePokedexNumber === selectedPokemon ? 'nav-card-selected' : ' ')}
           >
             <p>{getFullPokedexNumber(truePokedexNumber)}</p>
